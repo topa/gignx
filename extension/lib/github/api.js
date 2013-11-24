4c63b24153c92ae27ce5e9b1b5c40b62c271c040
Paul Torka <paul.torka@peerigon.com>
2013-11-24 15:44:35 +0100

    Enhancement: If a autoRefreshInterval is given to getNotifications as second param, then it will auto refresh notifications within this interval

--- extension/lib/github/api.js
+++ extension/lib/github/api.js
@@ -1,13 +1,22 @@
 
 const Lang = imports.lang;
 const Soup = imports.gi.Soup;
+const Mainloop = imports.mainloop;
 
 const GitHubAPIURL = "https://api.github.com";
 
+/**
+ * @type {Lang.Class}
+ * @constructor
+ */
 const GitHubAPI = new Lang.Class({
 
     Name: 'GitHubAPI',
 
+    /**
+     * @param {string} access_token
+     * @protected
+     */
     _init: function (access_token) {
         this._access_token = access_token;
 
@@ -15,27 +24,69 @@ const GitHubAPI = new Lang.Class({
         Soup.Session.prototype.add_feature.call(this._httpSession, new Soup.ProxyResolverDefault());
     },
 
+    /**
+     * @type {Soup.SessionAsync}
+     * @protected
+     */
     _httpSession: null,
 
+    /**
+     * @type {{ssl_use_system_ca_file: boolean}}
+     * @protected
+     */
     _httpSessionOptions: {
         ssl_use_system_ca_file: true
     },
 
+    /**
+     * @type {string}
+     * @protected
+     */
     _access_token: "",
 
-    getNotifications: function (callback) {
+    /**
+     * @type {*|null}
+     */
+    getNotificationsLoop: null,
+
+    /**
+     * @param {function(Error|null, Array.<{}>?)} callback
+     * @param {number?} autoRefreshInterval optional
+     */
+    getNotifications: function (callback, autoRefreshInterval) {
         let url = GitHubAPIURL+"/notifications";
         // @see http://developer.github.com/v3/activity/notifications/
         let params = {
             access_token: this._access_token//,
-            //all: "1",
-            //participating: true,
-            //since: "2012-10-09T23:39:01Z"
+//            all: "1",
+//            participating: true,
+//            since: "2012-10-09T23:39:01Z"
         };
 
+        if (autoRefreshInterval) {
+            if (this.getNotificationsLoop) {
+                Mainloop.source_remove(this.getNotificationsLoop);
+            }
+
+            this.getNotificationsLoop = Mainloop.timeout_add(autoRefreshInterval, Lang.bind(this, function () {
+                this._get(url, params, callback);
+                params.since = new Date().toISOString();
+
+                return true;
+            }));
+        }
+
         this._get(url, params, callback);
+        // This will persist in getNotificationsLoop
+        params.since = new Date().toISOString();
     },
 
+    /**
+     * @param {string} url
+     * @param {{}} params
+     * @param {function(Error|null, Array.<{}>?)} callback
+     * @protected
+     */
     _get: function (url, params, callback) {
         let message = Soup.form_request_new_from_hash('GET', url, params);
 
@@ -43,7 +94,7 @@ const GitHubAPI = new Lang.Class({
             let parsedResponse, error;
 
             if (message.status_code == Soup.KnownStatusCode.OK) {
-                parsedResponse = this._parseResponse(message);
+                parsedResponse = this._parseResponse(message.response_body.data);
                 callback(null, parsedResponse);
             } else {
                 error = this._createAPICallError("GET", message.status_code);
@@ -52,8 +103,13 @@ const GitHubAPI = new Lang.Class({
         }));
     },
 
-    _parseResponse: function (message) {
-        let parsedResponse = JSON.parse(message.response_body.data);
+    /**
+     * @param {string} data
+     * @returns {{}}
+     * @protected
+     */
+    _parseResponse: function (data) {
+        let parsedResponse = JSON.parse(data);
 
         return parsedResponse;
     },
@@ -74,6 +130,10 @@ const GitHubAPI = new Lang.Class({
         }
 
         return error;
+    },
+
+    stopAllAutoRefreshLoops: function () {
+        Mainloop.source_remove(this.getNotificationsLoop);
     }
 
 });
