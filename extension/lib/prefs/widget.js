0bcc5e80da090db53696ed5e5e439a84e4c5c955
Paul Torka <paul.torka@peerigon.com>
2013-11-26 00:06:43 +0100

    Moved prefs-Widget into own Class in lib/prefs/Widget

---
+++ extension/lib/prefs/widget.js
@@ -0,0 +1,128 @@
+
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+/**
+ * @type {GObject.Class}
+ * @constructor
+ */
+const PrefsWidget = new GObject.Class({
+
+    Name: 'Gignx.Prefs.Widget',
+    GTypeName: 'GignxPrefsWidget',
+    Extends: Gtk.Box,
+
+    /**
+     * @TODO Check type of orientation
+     * @type {{orientation: string, border_width: number, spacing: number}}
+     */
+    _defaultParams: {
+        orientation: Gtk.Orientation.VERTICAL,
+        border_width: 10,
+        spacing: 10
+    },
+
+    /**
+     * @type {{margin_left: number, margin_top: number, margin_right: number, spacing: number}}
+     */
+    _defaultPrefParams: {
+        margin_left: 10,
+        margin_top: 10,
+        margin_right: 10,
+        spacing: 30
+    },
+
+    /**
+     *
+     * @param {PrefsSchema} PrefsSchema
+     * @protected
+     */
+    _init: function(PrefsSchema) {
+        this.parent(this._defaultParams);
+
+        this.add(this._createAutoRefreshSwitchPref(PrefsSchema));
+        // @TODO Disable if AutoRefreshSwitch was moved to false;
+        this.add(this._createAutoRefreshIntervalPref(PrefsSchema));
+        this.add(this._createAccessTokenPref(PrefsSchema));
+    },
+
+    /**
+     * @param {PrefsSchema} PrefsSchema
+     * @returns {Gtk.Box}
+     * @protected
+     */
+    _createAutoRefreshSwitchPref: function (PrefsSchema) {
+        let box = new Gtk.Box(this._defaultPrefParams);
+        let label = new Gtk.Label({
+            label: "Enable/Disable Auto-Refresh",
+            xalign: 0
+        });
+        let switcher = new Gtk.Switch({
+            active: PrefsSchema.isAutoRefresh()
+        });
+
+        // update
+        switcher.connect("notify::active", function () {
+            PrefsSchema.isAutoRefresh(switcher.get_active());
+        });
+
+        box.add(label);
+        box.add(switcher);
+
+        return box;
+    },
+
+    /**
+     * @param {PrefsSchema} PrefsSchema
+     * @returns {Gtk.Box}
+     * @protected
+     */
+    _createAutoRefreshIntervalPref: function (PrefsSchema) {
+        let box = new Gtk.Box(this._defaultPrefParams);
+        let label = new Gtk.Label({
+            label: "Auto-Refresh-Interval (minutes)",
+            xalign: 0
+        });
+        let spinButton = new Gtk.SpinButton();
+        spinButton.set_range(1, 120);
+        spinButton.set_increments(1, 5);
+        spinButton.set_value(PrefsSchema.autoRefreshInterval());
+
+        // update
+        spinButton.connect("value-changed", function () {
+            PrefsSchema.autoRefreshInterval(spinButton.get_value());
+        });
+
+        box.add(label);
+        box.add(spinButton);
+
+        return box;
+    },
+
+    /**
+     * @param {PrefsSchema} PrefsSchema
+     * @returns {Gtk.Box}
+     * @protected
+     */
+    _createAccessTokenPref: function (PrefsSchema) {
+        let box = new Gtk.Box(this._defaultPrefParams);
+        // @TODO Create a link to https://github.com/settings/applications
+        let label = new Gtk.Label({
+            label: "Personal Access Tokens",
+            xalign: 0
+        });
+        let entry = new Gtk.Entry({
+            text: PrefsSchema.accessToken()
+        });
+        entry.connect("changed", function () {
+            PrefsSchema.accessToken(entry.get_text());
+        });
+
+        box.add(label);
+        box.add(entry);
+
+        return box;
+    }
+
+});
--- extension/prefs.js
+++ extension/prefs.js
@@ -1,163 +1,17 @@
-// -*- mode: js2; indent-tabs-mode: nil; js2-basic-offset: 4 -*-
 
-const GLib = imports.gi.GLib;
-const GObject = imports.gi.GObject;
-const Gio = imports.gi.Gio;
-const Gtk = imports.gi.Gtk;
-const Lang = imports.lang;
+const gignx = imports.misc.extensionUtils.getCurrentExtension();
+const prefs = gignx.imports.lib.prefs;
 
-const Gettext = imports.gettext.domain('gnome-shell-extensions');
-const _ = Gettext.gettext;
-
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
+const PrefsWidget = prefs.widget.PrefsWidget;
+const PrefsSchema = prefs.schema.PrefsSchema;
 
 function init() {
-//    Convenience.initTranslations();
+    //@TODO Init Translation here
 }
 
-/**
- * @type {GObject.Class}
- * @constructor
- */
-const GignxPrefsWidget = new GObject.Class({
-
-    Name: 'Gignx.Prefs.Widget',
-    GTypeName: 'GignxPrefsWidget',
-    Extends: Gtk.Box,
-
-    /**
-     * @type {Gio.Settings|null}
-     */
-    _settings: null,
-
-    /**
-     * @type {Gtk.Box|null}
-     * @protected
-     */
-    _autoRefreshSwitchSetting: null,
-
-    /**
-     * @type {Gtk.Box|null}
-     * @protected
-     */
-    _autoRefreshIntervalSetting: null,
-
-    /**
-     * @type {Gtk.Box|null}
-     * @protected
-     */
-    _authorizationTokenSetting: null,
-
-    _init: function(params) {
-        this.parent(params);
-//        this.margin = this.row_spacing = this.column_spacing = 10;
-
-        this._initSettings();
-        this._initAutoRefreshSwitchSetting();
-        this._initAutoRefreshIntervalSetting();
-        this._initAuthorizationTokenSetting();
-    },
-
-    _initSettings: function () {
-        this._settings = Convenience.getSettings();
-    },
-
-    _initAutoRefreshSwitchSetting: function () {
-        this._autoRefreshSwitchSetting = new Gtk.Box({
-            spacing: 30,
-            margin_left: 10,
-            margin_top: 10,
-            margin_right: 10
-        });
-        let label = new Gtk.Label({
-            label: "Enable/Disable Auto-Refresh",
-            use_markup: true,
-            xalign: 0,
-            hexpand:true
-        });
-        let isautoRefreshSwitchActive = this._settings.get_boolean("enable-auto-refresh");
-        let autoRefreshSwitch = new Gtk.Switch({halign:Gtk.Align.END});
-        // init
-        autoRefreshSwitch.set_active(isautoRefreshSwitchActive);
-        // update
-        autoRefreshSwitch.connect("notify::active", Lang.bind(this, function () {
-             this._settings.set_boolean("enable-auto-refresh", autoRefreshSwitch.get_active());
-        }));
-
-        this._autoRefreshSwitchSetting.add(label);
-        this._autoRefreshSwitchSetting.add(autoRefreshSwitch);
-
-        this.add(this._autoRefreshSwitchSetting);
-    },
-
-    _initAutoRefreshIntervalSetting: function () {
-        this._autoRefreshIntervalSetting = new Gtk.Box({
-            spacing: 30,
-            margin_left: 10,
-            margin_top: 10,
-            margin_right: 10
-        });
-        let label = new Gtk.Label({
-            label: "Authorization-Token",
-            use_markup: true,
-            xalign: 0,
-            hexpand:true
-        });
-        let autoRefreshIntervalRange = new Gtk.Scale({
-            orientation: Gtk.Orientation.HORIZONTAL,
-            valuePos: Gtk.PositionType.RIGHT
-        });
-        // init
-        autoRefreshIntervalRange.set_range(1, 1440);
-        autoRefreshIntervalRange.set_value(this._settings.get_int('auto-refresh-interval'));
-        autoRefreshIntervalRange.set_increments(5, 5);
-        autoRefreshIntervalRange.set_size_request(200, -1);
-        // update
-        autoRefreshIntervalRange.connect("value-changed", Lang.bind(this, function () {
-            this._settings.set_int("auto-refresh-interval", autoRefreshIntervalRange.get_value());
-        }));
-
-        this._autoRefreshIntervalSetting.add(label);
-        this._autoRefreshIntervalSetting.add(autoRefreshIntervalRange);
-
-        this.add(this._autoRefreshIntervalSetting);
-    },
-
-    _initAuthorizationTokenSetting: function () {
-        this._authorizationTokenSetting = new Gtk.Box({
-            spacing: 30,
-            margin_left: 10,
-            margin_top: 10,
-            margin_right: 10
-        });
-        let label = new Gtk.Label({
-            label: "Auto-Refresh-Interval",
-            use_markup: true,
-            xalign: 0,
-            hexpand:true
-        });
-        let authorizationTokenEnty = new Gtk.Entry({
-            hexpand: true
-        });
-        authorizationTokenEnty.set_text(this._settings.get_string("github-api-authentication-token"));
-        authorizationTokenEnty.connect("changed", Lang.bind(this, function () {
-            this._settings.set_string("github-api-authentication-token", authorizationTokenEnty.get_text());
-        }));
-
-        this._authorizationTokenSetting.add(label);
-        this._authorizationTokenSetting.add(authorizationTokenEnty);
-
-        this.add(this._authorizationTokenSetting);
-    }
-
-});
-
-
 function buildPrefsWidget() {
-    let widget = new GignxPrefsWidget();
-    widget.show_all();
+    let prefsWidget = new PrefsWidget(PrefsSchema);
+    prefsWidget.show_all();
 
-    return widget;
+    return prefsWidget;
 }
