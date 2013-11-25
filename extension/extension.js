9bfed30d5321d543ee751cea6c83e717ef2a1144
Paul Torka <paul.torka@peerigon.com>
2013-11-26 00:07:38 +0100

    Using new PrefsSchema

--- extension/extension.js
+++ extension/extension.js
@@ -3,25 +3,20 @@ const Main = imports.ui.main;
 const IconTheme = imports.gi.Gtk.IconTheme;
 
 const gignx = imports.misc.extensionUtils.getCurrentExtension();
+const lib = gignx.imports.lib;
 
 ////////////
 // GitHub //
 ////////////
-const libGithub = gignx.imports.lib.github;
-const GitHubAPI = libGithub.api.GitHubAPI;
-const GitHubNotifications = libGithub.notifications.GitHubNotifications;
+const GitHubAPI = lib.github.api.GitHubAPI;
+const GitHubNotifications = lib.github.notifications.GitHubNotifications;
 let gitHubNotifications;
 let gitHubApi;
 
 //////////////
 // Settings //
 //////////////
-const Convenience = gignx.imports.convenience;
-let settings = Convenience.getSettings();
-let authenticationToken = settings.get_string("github-api-authentication-token");
-let isAutoRefreshEnabled = settings.get_boolean("enable-auto-refresh");
-let autoRefreshInterval = parseInt(settings.get_int("auto-refresh-interval") * 60 * 1000, 10);
-
+const PrefsSchema = lib.prefs.schema.PrefsSchema;
 
 
 /**
@@ -42,18 +37,17 @@ function onNotificationsFetched(error, notifications) {
  * @param {{}} extensionMeta
  */
 function init(extensionMeta) {
+    let autoRefreshInterval = PrefsSchema.isAutoRefresh() ? (PrefsSchema.autoRefreshInterval() * 60 * 1000) : null;
     let iconTheme = IconTheme.get_default();
+
     iconTheme.append_search_path(extensionMeta.path + "/icons");
 
-    gitHubApi = new GitHubAPI(authenticationToken);
+    gitHubApi = new GitHubAPI(PrefsSchema.accessToken());
     gitHubNotifications = new GitHubNotifications();
 
-    if (isAutoRefreshEnabled) {
+    if (PrefsSchema.accessToken()) {
         gitHubApi.getNotifications(onNotificationsFetched, autoRefreshInterval);
-    } else {
-        gitHubApi.getNotifications(onNotificationsFetched);
     }
-
 }
 
 function enable() {
