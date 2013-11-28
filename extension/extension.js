605f69020a853d49e3d7068bea38a6977dc61b87
Paul Torka <paul.torka@peerigon.com>
2013-11-28 22:25:17 +0100

    - Uses LoopRegistry now - @matthaias

--- extension/extension.js
+++ extension/extension.js
@@ -18,44 +18,58 @@ let gitHubApi;
 //////////////
 const PrefsSchema = lib.prefs.schema.PrefsSchema;
 
+////////////////////
+// handling Loops //
+////////////////////
+const LoopRegistry = lib.loop.registry.LoopRegistry;
+let loopRegistry;
 
-/**
- * @param {Error|null} error
- * @param notifications
- */
-function onNotificationsFetched(error, notifications) {
-    if (error) {
-        //@TODO error handling
-        global.log(new Date().toString(), error);
-    } else {
-        global.log(new Date().toString(), notifications);
-        gitHubNotifications.addEntries(notifications);
-    }
-}
 
 /**
  * @param {{}} extensionMeta
  */
 function init(extensionMeta) {
-    let iconTheme = IconTheme.get_default();
+    let iconTheme;
+    let autoRefreshInterval;
 
+    iconTheme = IconTheme.get_default();
     iconTheme.append_search_path(extensionMeta.path + "/icons");
-}
 
-function enable() {
-    let autoRefreshInterval = PrefsSchema.isAutoRefresh() ? (PrefsSchema.getAutoRefreshInterval() * 60 * 1000) : null;
+    autoRefreshInterval = PrefsSchema.isAutoRefresh() ? (PrefsSchema.getAutoRefreshInterval() * 60 * 1000) : null;
 
     gitHubApi = new GitHubAPI(PrefsSchema.getAccessToken());
     gitHubNotifications = new GitHubNotifications();
 
-    if (PrefsSchema.getAccessToken()) {
-        gitHubApi.getNotifications(onNotificationsFetched, autoRefreshInterval);
+    /**
+     * @param {Error|null} error
+     * @param {Array.<{}>} notifications
+     */
+    function onNotificationsFetched(error, notifications) {
+        if (error) {
+            //@TODO error handling
+            global.log(new Date().toString(), error);
+        } else {
+            global.log(new Date().toString(), notifications);
+            gitHubNotifications.addEntries(notifications);
+        }
     }
 
+    function fetchNotifications() {
+        if (PrefsSchema.getAccessToken()) {
+            gitHubApi.getNotifications(onNotificationsFetched);
+        }
+    }
+
+    loopRegistry = new LoopRegistry();
+    loopRegistry.add(autoRefreshInterval, fetchNotifications, true);
+}
+
+function enable() {
     Main.panel.addToStatusArea("gignx-github-notification-overview", gitHubNotifications);
+    loopRegistry.startAll();
 }
 
 function disable() {
     gitHubNotifications.destroy();
-    gitHubApi.stopAllAutoRefreshLoops();
+    loopRegistry.stopAll();
 }
