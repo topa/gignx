55774231f2fceadf786c2267adb2fa4431d2099f
Paul Torka <paul.torka@peerigon.com>
2013-11-30 16:34:34 +0100

    Enhancement: gignx is now able to listen on changes in prefs. Enhancenment/Tmp: Added some more logging Enhancement: Prefixed logs with "(gignx)" Notify: @matthaias

--- extension/extension.js
+++ extension/extension.js
@@ -10,8 +10,6 @@ const lib = gignx.imports.lib;
 ////////////
 const GitHubAPI = lib.github.api.GitHubAPI;
 const GitHubNotifications = lib.github.notifications.GitHubNotifications;
-let gitHubNotifications;
-let gitHubApi;
 
 //////////////
 // Settings //
@@ -22,58 +20,114 @@ const PrefsSchema = lib.prefs.schema.PrefsSchema;
 // handling Loops //
 ////////////////////
 const LoopRegistry = lib.loop.registry.LoopRegistry;
-let loopRegistry;
+
+
+/**
+ * @type {GitHubNotifications|null}
+ */
+let gitHubNotifications = null;
+/**
+ * @type {GitHubAPI|null}
+ */
+let gitHubApi = null;
+
+/**
+ * @type {LoopTask|null}
+ */
+let fetchNotificationsLoopTask;
+
+/**
+ * @type {{since: string}}
+ */
+let fetchNotificationsParams = { since: "" };
 
 
 /**
  * @param {{}} extensionMeta
  */
 function init(extensionMeta) {
-    let iconTheme;
-    let autoRefreshInterval;
-    let getNotificationParams = {};
 
-    iconTheme = IconTheme.get_default();
-    iconTheme.append_search_path(extensionMeta.path + "/icons");
+    IconTheme.get_default().append_search_path(extensionMeta.path + "/icons");
 
-    autoRefreshInterval = PrefsSchema.isAutoRefresh() ? (PrefsSchema.getAutoRefreshInterval() * 60 * 1000) : null;
+    PrefsSchema.connect("changed", _initGitHubNotifications);
 
-    gitHubApi = new GitHubAPI(PrefsSchema.getAccessToken());
-    gitHubNotifications = new GitHubNotifications();
-
-    /**
-     * @param {Error|null} error
-     * @param {Array.<{}>} notifications
-     */
-    function onNotificationsFetched(error, notifications) {
-        if (error) {
-            //@TODO error handling
-            global.log(new Date().toString(), error);
-        } else {
-            global.log(new Date().toString(), notifications);
-            gitHubNotifications.addEntries(notifications);
-        }
-    }
-
-    function fetchNotificationsTask() {
-        if (PrefsSchema.getAccessToken()) {
-            gitHubApi.getNotifications(onNotificationsFetched, getNotificationParams);
-            // Omit notifications which were already fetched
-            getNotificationParams.since = new Date().toISOString();
-        }
-        return true;
-    }
-
-    loopRegistry = new LoopRegistry();
-    loopRegistry.add(autoRefreshInterval, fetchNotificationsTask, true);
 }
 
+/**
+ * Adds GitHubNotification indicator to panel, fetches initially notifications from GitHub and enables added LoopTasks
+ */
 function enable() {
+    _initGitHubNotifications();
     Main.panel.addToStatusArea("gignx-github-notification-overview", gitHubNotifications);
-    loopRegistry.startAll();
+    LoopRegistry.startAll();
 }
 
 function disable() {
     gitHubNotifications.destroy();
-    loopRegistry.stopAll();
+    gitHubNotifications = null;
+    LoopRegistry.stopAll();
+}
+
+/**
+ * Initializes GitHubNotifications indicator and enables access to GitHub's API.
+ * However this is a _init-method it is meant to be called multiple times to be able to react on changes in prefs.
+ */
+function _initGitHubNotifications() {
+
+    let accessToken = PrefsSchema.getAccessToken();
+    let isAccessToken = !!accessToken;
+    let isAutoRefresh = PrefsSchema.isAutoRefresh();
+    let autoRefreshInterval = PrefsSchema.getAutoRefreshInterval() * 60 * 1000; //make milliseconds
+
+    // Make sure there is at max one at least none fetchNotificationTask running
+    if (fetchNotificationsLoopTask) {
+        LoopRegistry.remove(fetchNotificationsLoopTask);
+    }
+
+    // Create indicator only once
+    if (!gitHubNotifications) {
+        gitHubNotifications = new GitHubNotifications();
+    }
+
+    if (isAccessToken) {
+        global.log("(gignx) Authentication token is set. Will fetch new notifications.");
+        gitHubApi = new GitHubAPI(accessToken);
+    // That's it without accessToken
+    } else {
+        global.log("(gignx) No authentication token was set. Won't fetch any notifications.");
+        gitHubApi = null;
+        return;
+    }
+
+    if (isAutoRefresh) {
+        global.log("(gignx) Auto-refresh is enabled. Will fetch notifications every "+autoRefreshInterval+"ms.");
+        fetchNotificationsLoopTask = LoopRegistry.add(autoRefreshInterval, _fetchNotificationsTask);
+    }
+
+    _fetchNotificationsTask();
+}
+
+/**
+ * Fetches notifications from GitHub and adds them to GitHubNotifications indicator
+ * @returns {boolean}
+ */
+function _fetchNotificationsTask() {
+    gitHubApi.getNotifications(_onNotificationsFetched, fetchNotificationsParams);
+    fetchNotificationsParams.since = new Date().toISOString();
+
+    return true;
+}
+
+/**
+ * @param {Error|null} error
+ * @param {Array.<{}>} notifications
+ */
+function _onNotificationsFetched(error, notifications) {
+    if (error) {
+        //@TODO error handling
+        global.log("(gignx)", new Date().toString(), "An Error has occurred while fetching notifications:", error);
+    } else {
+        global.log("(gignx)", new Date().toString(), "Fetched "+notifications.length+" new notifications");
+        gitHubNotifications.addEntries(notifications);
+    }
 }
